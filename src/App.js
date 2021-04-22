24d6e8abb9b29034f72d5cfa11b0a417ef28ff06
Francois Polo <andasan@gmail.com>
2021-04-22 10:10:08 -0700

    completed login functionality

--- src/App.js
+++ src/App.js
@@ -3,9 +3,32 @@ import { useSelector, useDispatch} from 'react-redux'
 
 import { selectUser, login, logout } from './features/userSlice'
 import {auth} from './firebase/firebase'
+import Login from './components/Login';
 
 const App = () => {
-  return ( <> </> )
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if(authUser){
+        dispatch(login({
+          uid: authUser.uid,
+          photo: authUser.photoURL,
+          email: authUser.email,
+          displayName: authUser.displayName
+        }))
+      }else {
+        dispatch(logout())
+      }
+    })
+  }, [dispatch])
+
+  return (
+    <>
+      <Login />
+    </>
+  )
 }
 
 export default App
